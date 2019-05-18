31b6aa5aaad441b40c1273521e8bfe5bb6e0ac98
Usuario <Usuario@DESKTOP-7IM02CJ>
2019-05-17 21:45:34 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+    
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int num2 = num;
+            int num3 = 0;
+            
+            for (num=0 ; num<= 40; num++)
+            {
+
+                Console.WriteLine("la serie es " + num + "+" + num3 + " = " + num2 );
+                num2 = num2 + num;
+                num3 = num;
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static double numero ()
+        {
+            double grados;
+            Console.WriteLine("Ingrese grados Centigrados");
+            grados = Convert.ToDouble(Console.ReadLine());
+            return grados;
+
+        }
+        static void Main(string[] args)
+        {
+            double grados;
+            double far = 0;
+   
+            grados = numero();
+            Console.WriteLine("Los grados Centigrados ingresados son " + grados + "°" + "\n");
+            double fa = ((9.0 / 5.0) * grados + 32);
+            Console.WriteLine("Los Grados Ingresados anteriormente convertidos a Fahrenheit son " + fa  +"°" + "\n" );
+            Console.ReadKey();
+
+        }
+    }
+}
