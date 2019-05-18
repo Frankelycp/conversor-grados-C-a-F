6789a186ca79203238ec437eedeeb6190b43c5cb
frankelyCP <frankelycp@gmail.com>
2019-05-18 15:55:16 -0300

    12

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -30,7 +30,7 @@ namespace ConversorGradosCaF
                 Console.ReadKey();
             }
             else
-                Console.WriteLine("Ingrese un numero mayor a 0 ");
+                Console.WriteLine("Ingrese un numero mayor a 01");
             Console.ReadKey();
         }
  
